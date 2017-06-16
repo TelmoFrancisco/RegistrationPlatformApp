d3c6427c784da00750374be48c78b4403559261c
Telmo Francisco <telmo.rodrigues.francisco@celfocus.com>
2017-06-16 17:42:31 +0100

    bootswatch add

---
+++ src/main/webapp/app/components/bootswatch/bootswatch.service.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+
+'use strict';
+
+angular.module('yourApp')
+    .factory('BootSwatchService', function ($http) {
+        return {
+            get: function() {
+                return $http.get('http://bootswatch.com/api/3.json').then(function (response) {
+                    return response.data.themes;
+                });
+            }
+        };
+    });
